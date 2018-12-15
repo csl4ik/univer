78efa4415d481e0f753cb2a54e0e4c28353d42fe
Asik <42998694+csl4ik@users.noreply.github.com>
2018-12-15 18:45:40 +0300

    Create labka10.js

---
+++ javascript/labka10.js
@@ -0,0 +1,41 @@
+/*
+var d,r,sum=0 //207
+d=+prompt ("Введи число",'') // сохраним в d введенное число
+while ( d!=0 )
+{
+r = d % 10 // найдем целый остаток от деления числа на 10 2%10
+sum = sum + r
+d = Math.floor(d / 10) // округлим d / 10 до целого числа
+console.log("d= "+d);
+console.log("sum= "+sum);
+console.log("r= "+r);
+}
+console.log("sum "  + sum)
+*/
+/*
+//1
+var i=0
+while (i<3) {
+  console.log("i "+i);
+  i++
+}
+*/
+/*
+//2
+var i=100
+while (i>=0) {
+  console.log("i "+i);
+  i=i-20
+}
+*/
+/*
+//3
+var i=-1, A=[], sum=0
+while (A[i]!==0) {
+    i++
+  A[i]=+prompt("введите A["+i+"]=","")
+}
+for(i=0;i<A.length;i++){
+  sum+= +A[i]}
+  console.log("sum = "+sum);
+*/
